ea76d85b14057696a39a36b18f3d47c74fec23e4
Indranjana <112919974+IndranjanaChatterjee@users.noreply.github.com>
2024-04-10 19:17:59 +0530

    Update Hello.java

--- Ass1/Hello.java
+++ Ass1/Hello.java
@@ -1,24 +1,8 @@
-/**
- * Class Hello:
- *
- * Hello-world program to demonstrate BlueJ.
- */
-class Hello
+import java.util.*;
+public class print
 {
-    /**
-     * Method that does the work 
-     */
-    public void go()
+    public static void main(String args[])
     {
-        System.out.println("Hello, world");
-    }
-    
-    /**
-     *  main method for testing outside BlueJ 
-     */
-    public static void main(String[] args)
-    {
-        Hello hi = new Hello();
-        hi.go();
+        System.out.println("Hello Java");
     }
 }
