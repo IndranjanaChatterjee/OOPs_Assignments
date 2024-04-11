6864e9b174326a3f7361def49e19b2b70a5afdff
Indranjana <112919974+IndranjanaChatterjee@users.noreply.github.com>
2024-04-11 08:20:07 +0530

    Update ArraySum.java

--- Assignment2/ArraySum.java
+++ Assignment2/ArraySum.java
@@ -1,12 +1,12 @@
 import java.util.*;
-public class DiaSum
+public class ArraySum
 {
     public static void main(String args[])
     {
         int a[][]=new int[3][3];
         int b[][]=new int[3][3];
         int c[][]=new int[3][3];
-        int i,j;
+        int i,j,k,sum=0;
         Scanner in=new Scanner(System.in);
         for(i=0;i<3;i++)
         {
@@ -15,22 +15,28 @@ public class DiaSum
                 a[i][j]=in.nextInt();
             }
         }
-        int rsum=0,lsum=0;
         for(i=0;i<3;i++)
         {
             for(j=0;j<3;j++)
             {
-                if(i==j)
-                {
-                    lsum+=a[i][j];
-                }
-                if((i+j)==2)
-                {
-                    rsum+=a[i][j];
-                }
+                b[i][j]=in.nextInt();
             }
         }
-        System.out.println(lsum);
-        System.out.println(rsum);
+        
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                c[i][j]=a[i][j]+b[i][j];
+            }
+        }
+        for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
